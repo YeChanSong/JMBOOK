55c9100f9b8d280d0d0b52f8ed8b8d69731e89f4
YeChanSong <davidsyc1170@gmail.com>
2020-08-07 20:54:09 +0900

    시간초과 수정
    오답 원인 찾기

--- JMBook/Chapter1/FESTIVAL.py
+++ JMBook/Chapter1/FESTIVAL.py
@@ -9,28 +9,32 @@ for i in range(C):
     L.append(l)
     cst = list(map(int,input().split()))
     cost.append(cst)
-
-print(cost)
+#print(C,N,L)
+#print(cost)
 # 각 케이스 계산
 for i in range(C):
     low = 1000.0
     for j in range(L[i],N[i]+1):
         # 주어진 비용들을 L부터 N까지 순회하며 최소 평균 구하기
         li = cost[i][:j]
+        tmpsum = sum(li)
         ptr = j
 
         while 1:
-            #print(li)
+            print(li)
+            #print(tmpsum/j)
             if ptr == len(cost[i]):
                 break
 			
-            tmp = sum(li)/j
+            tmp = tmpsum/j
             if low > tmp:
                 low = tmp
 			
+            tmpsum -= li[0]
             del(li[0])
             #print(ptr)
             
             li.append(cost[i][ptr])
+            tmpsum += li[-1]
             ptr +=1
-    print("%.12f" %low)
+    print("%.11f" %low)
