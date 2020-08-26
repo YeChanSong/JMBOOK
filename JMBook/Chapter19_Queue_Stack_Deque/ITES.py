f7d77b33670c8fffe4bf692ba3d09bd45a05d047
YeChanSong <davidsyc1170@gmail.com>
2020-08-26 20:48:09 +0900

    ITES 시간초과22

--- JMBook/Chapter19_Queue_Stack_Deque/ITES.py
+++ JMBook/Chapter19_Queue_Stack_Deque/ITES.py
@@ -12,42 +12,7 @@ def signalGen():
 # 제너레이터로 신호생성
 
 C = int(input())
-# 테스트 케이스의 수 입력C = int(input())
 # 테스트 케이스의 수 입력
-
-for i in range(C):
-
-    K, N = map(int,input().split())
-    tmp = []
-    t2mp = 1983
-    for j in range(1,N+1):
-        tmp.append(t2mp%10000 +1)
-        t2mp = (t2mp * 214013 + 2531011) % 2**32
-        #print(t2mp)
-        # t2mp에 신호 기록 생성해서 저장
-        
-        # tmp에 입력 신호 생성해서 저장
-    #print(tmp)
-    # 여기까지 입력 신호 생성
-    cnt = 0
-    # 부분 수열의 합이 K값과 일치할 경우 cnt 증가
-    
-    for k in range(1,N+1):
-        subtotal = 0
-        idx = 0
-        for a in range(k):
-            subtotal += tmp[a]
-            idx = a
-        # 최초 k 크기 만큼의 부분 합
-        for a in range(len(tmp)):
-            if subtotal == K:
-                cnt += 1
-            idx += 1
-            if idx == len(tmp):
-                break
-            subtotal -= tmp[a]
-            subtotal += tmp[idx]
-    print(cnt)
 
 for i in range(C):
 
