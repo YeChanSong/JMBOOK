f4f119f5305937bfa3ccfbaf90cf75910e1b6063
YeChanSong <davidsyc1170@gmail.com>
2020-09-16 17:15:34 +0900

    PICNIC 중복처리 시간초과

--- JMBook/Chapter6_Brute_force/PICNIC.py
+++ JMBook/Chapter6_Brute_force/PICNIC.py
@@ -1,36 +1,48 @@
 import sys
 sys.setrecursionlimit(1000000)
-from copy import deepcopy
 
-def recur(friend,set):
+def recur(set,num):
     '''
-    friend: 남은 친구 관계를 나타내는 튜플 리스트
-    set: 친구 관계가 정해져야 할 학생들
-    반환되는 값은 가능한 조합의 수
+    set: 지정된 친구들
+    num: 짝을 지정할 친구
     '''
     total = 0
-    if len(set) == 0:
+    global n, friend
+    if num >= n:
+        return 0
+    if len(set) == n:
         return 1
-    # 학생들이 모두 분류된 경우 종료
-    tmpfriend = deepcopy(friend)
-    for i  in friend:
-        if i[0] in set and i[1] in set:
-            tmp = deepcopy(set)
-            tmp.remove(i[0])
-            tmp.remove(i[1])
-            #tmpfriend.remove(i)
-            #print(tmpfriend)
-            total += recur(tmpfriend,tmp)
-        tmpfriend.remove(i)
+    if num in set:
+        return recur(set,num+1)
+    
+    for i in range(num+1,n):
+        flag = False
+        if i not in set:
+            for j in range(len(friend)//2):
+                
+                if (friend[j*2] == num and friend[j*2+1] == i)\
+                    or (friend[j*2] == i and friend[j*2+1] == num):
+                        if flag == False:
+                            tmp = set.copy()
+                            tmp.append(num)
+                            tmp.append(i)
+                            #print(tmp)
+                            total += recur(tmp,num+1)
+                            flag = True
+        else:
+            total += recur(set,num+1)
+            
     return total
+    
+    
 
 C = int(input())
 
 for i in range(C):
+    global n, m
     n, m = map(int,input().split())
-    tmp = list(input().split())
-    friend = [(int(tmp[i*2]),int(tmp[i*2 +1])) for i in range(len(tmp)//2) ]
-    # 각 테스트케이스 입력 처리
-    #ret = recur(friend,[i for i in range(n)])
-    print(recur(friend,list(range(n))))
+    global friend
+    friend = list(map(int,input().split()))
+    empty = []
+    print(recur(empty,0))
 
