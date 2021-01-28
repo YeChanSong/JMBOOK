a2d2df1b90529e96158eb19abcd712e78ab4dd86
YeChanSong <davidsyc1170@gmail.com>
2021-01-28 18:47:21 +0900

    ROUTING
    1. 테스트 케이스의 수 만큼 반복을 안해서 오답.
    2. 시간초과가 많이 발생했는데,  int,float 변환을 자주하는 것도 큰 차이가 될 수 있음을 확인

--- Chapter30_Shortest_Path/ROUTING.py
+++ Chapter30_Shortest_Path/ROUTING.py
@@ -1,30 +1,32 @@
-import sys, heapq, math
-INF = 1e200
+import sys, heapq
+INF = float('inf')
 def dijkstra(idx):
     global adj, pq, N, INF
-
-    dist = list(INF for i in range(N))
-    heapq.heappush(pq,(0,idx))
-    while len(pq):
+    dist = [INF] * N
+    dist[idx] = 1.0
+    heapq.heappush(pq,(1.0,idx))
+    while pq:
         cost, here = heapq.heappop(pq)
         if dist[here] < cost:
             continue
-        for i in range(len(adj[here])):
-            there, nextDist = adj[here][i]
-            nextDist += cost
+        for there, nextDist in adj[here]:
+            nextDist *= cost
             if dist[there] > nextDist:
                 dist[there] = nextDist
                 heapq.heappush(pq,(nextDist,there))
     return dist
     
 C = int(sys.stdin.readline().strip())
-N,M = map(int,sys.stdin.readline().strip().split())
-adj = list([] for i in range(N))
-pq = list()
-for i in range(M):
-    s,d,n = sys.stdin.readline().strip().split()
-    adj[int(s)].append((int(d),math.log10(float(n))))
-    adj[int(d)].append((int(s),math.log10(float(n))))
+for k in range(C):
+    N,M = map(int,sys.stdin.readline().strip().split())
+    adj = list([] for i in range(N))
+    pq = list()
+    for i in range(M):
+        s,d,n = sys.stdin.readline().strip().split()
+        s,d = int(s), int(d)
+        n = float(n)
+        adj[s].append((d,n))
+        adj[d].append((s,n))
     
-dist = dijkstra(0)
-print("%0.10f"%10**dist[N-1])
+    dist = dijkstra(0)
+    print("%0.10f"%dist[N-1])
